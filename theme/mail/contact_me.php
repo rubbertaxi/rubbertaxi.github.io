8eec1864be7771b4e011d7587e573f9f9c090550
Phat Doan <phat@decen.io>
2017-10-07 09:57:57 +0700

    Update documentation

--- theme/mail/contact_me.php
+++ theme/mail/contact_me.php
@@ -2,53 +2,25 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-  // empty($_POST['phone']) 		||
+   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
-
+	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-//$phone = $_POST['phone'];
+$phone = $_POST['phone'];
 $message = $_POST['message'];
-$url = 'https://api.sendgrid.com/';
-$user = 'username';
-$pass = 'supersecretpassword';
-$messagebody = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$messagebodyhtml = "You have received a new message from your website contact form.<br>"."Here are the details:<br>Name: $name<br>Email: $email_address<br>Message: $message";
-$params = array(
-    'api_user'  => $user,
-    'api_key'   => $pass,
-    'to'        => 'you@yourdomain.com',
-    'subject'   => 'Someone sent a message from yourdomain.com',
-    'html'      => $messagebodyhtml,
-    'text'      => $messagebody,
-    'from'      => 'yourform@yourdomain.com',
-  );
-
-
-$request =  $url.'api/mail.send.json';
-
-// Generate curl request
-$session = curl_init($request);
-// Tell curl to use HTTP POST
-curl_setopt ($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
-curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
-curl_setopt($session, CURLOPT_HEADER, false);
-// Tell PHP not to use SSLv3 (instead opting for TLS)
-curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
-// obtain response
-$response = curl_exec($session);
-curl_close($session);
-
-// print everything out
-print_r($response);
-
+	
+// Create the email and send the message
+$to = 'ondoheer@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Ondoheer's Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
 ?>
